0848ba9ef7cf8a414143b8375171f80a74b73e12
Yes-Not-No <84343984+Yes-Not-No@users.noreply.github.com>
2021-10-03 18:45:15 +0700

    modele2-task1 исправлены ошибки

--- js/main.js
+++ js/main.js
@@ -1,28 +1,57 @@
-const getRandomInt = (min, max, length) => {
+function getRandomInteger(min, max) {
 
   //Проверка на отрицательные значения
 
-  const positiveMin = min >= 0 ? min : min * -1;
-  const positiveMax = max >= 0 ? max : max * -1;
+  let positiveMin = min >= 0 ? min : min * -1;
+  let positiveMax = max >= 0 ? max : max * -1;
 
   //Проверка 'max > min'
 
   if ( positiveMin > positiveMax ) {
 
-    const flippedMax = positiveMin;
-    const flippedMin = positiveMax;
-    const randomInt = String( Math.random() * (flippedMax - flippedMin) + flippedMin ).split('.');
-          randomInt[1] = randomInt[1].substr(0, length);
+    const container = positiveMax;
+    positiveMax = positiveMin;
+    positiveMin = container;
+  }
 
-    console.log( Number( randomInt.join('.')) );
-    return Number( randomInt.join('.'));
+  //Проверка 'max === min'
+
+  if ( positiveMin === positiveMax ) {
+
+    positiveMax++;
+  }
+
+  return Math.round( Math.random() * (positiveMax - positiveMin) + positiveMin );
+}
+
+function getRandomFractional(min, max, length) {
+
+  //Проверка на отрицательные значения
+
+  let positiveMin = min >= 0 ? min : min * -1;
+  let positiveMax = max >= 0 ? max : max * -1;
+
+  //Проверка 'max > min'
+
+  if ( positiveMin > positiveMax ) {
+
+    const container = positiveMax;
+    positiveMax = positiveMin;
+    positiveMin = container;
+  }
+
+  //Проверка 'max === min'
+
+  if ( positiveMin === positiveMax ) {
+
+    positiveMax++;
   }
 
   const randomInt = String( Math.random() * (positiveMax - positiveMin) + positiveMin ).split('.');
-        randomInt[1] = randomInt[1].substr(0, length);
+  randomInt[1] = randomInt[1].substr(0, length);
 
-  console.log( Number( randomInt.join('.')) );
-  return Number( randomInt.join('.'));
+  return Number( randomInt.join('.') );
 }
 
-getRandomInt(-1000, -100, 3)
+getRandomInteger(1, -100);
+getRandomFractional(1.2, 1.25, 3);
